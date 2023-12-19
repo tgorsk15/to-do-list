8a2b696b471eb98a931c296f9a40a32cef63c722
tgorsk15 <tgbusiness463@gmail.com>
2023-12-19 08:57:41 -0600

    Plan out structure of program and create js files
    COmments were added to index.js to plan out how the program will work. Additionally, two js files
    were created, one for the structure of the site, one to create objects associated with
    each task/project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './to-do.css';
 
+// objectCreator.js: fiile that contains the logic that creates
+// a new task or project
+
+// structure.js: file that lays out the foundational references
+// and links of the to-do structure
+
+// userform.js: handles the logic of what happens when the user
+// fills out a form to create a new project or task
+
+// listModification.js: handles the logic to display changes
+// on broswer when projects/tasks are added/deleted
+
+
+
+
 console.log('test');
